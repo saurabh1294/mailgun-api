4f0daba28a9654a3072f47f158713653d4ed075c
Saurabh Gattani <saurabh.gattani@gmail.com>
2019-05-26 13:06:00 +1000

    Error handling improvised

--- src/app/shared/services/mailgun-api.service.ts
+++ src/app/shared/services/mailgun-api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root' // singleton service
@@ -10,21 +10,27 @@ import { catchError } from 'rxjs/operators';
 export class MailgunAPIService {
   port = '3456';
   baseUrl = `http://localhost:${this.port}`;
+  numRetries = 3;
+
   constructor(private http: HttpClient) {}
 
-  sendMails(data): Observable<any> {
-    console.log(data.split('\n'));
-    /*return this.http.get<any>(`${this.baseUrl}/sendEmails?mailIds = ${data}`).pipe(
-      catchError(err => {
-        console.log('Handling service error locally and rethrowing it...', err);
-        return throwError(err);
-      })
-    )};*/
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
+  sendMails(data): Observable<any> {
+    console.log('Request payload', data.split('\n'));
     return this.http.post<any>(`${this.baseUrl}/sendMails`, data.split('\n')).pipe(
-      catchError(err => {
-        console.log('Handling service error locally and rethrowing it...', err);
-        return throwError(err);
-      })
+      retry(this.numRetries), // Retry thrice before ending request
+      catchError(err => this.handleError(err))
     )};
 }
